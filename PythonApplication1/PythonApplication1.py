490f98bfacd896c542eede876eb3a1f223a31579
Chris Huntley <ifthenelsenull@gmail.com>
2016-09-21 19:36:23 -0500

    adding sqllite db

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+
+con = lite.connect('test.db')
+
+scifi = input ("Is this a Scifi book? y/n :")
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute ("DROP TABLE IF EXISTS Book_classifier;")
+    cur.execute("CREATE TABLE Book_classifier(Id INT, Name TEXT, Price INT)")
+    cur.execute("INSERT INTO Book_classifier VALUES(1,'Audi',52642)")
+    cur.execute("INSERT INTO Book_classifier VALUES(2,'Mercedes',57127)")
+    cur.execute("INSERT INTO Book_classifier VALUES(3,'Skoda',9000)")
+    cur.execute("INSERT INTO Book_classifier VALUES(4,'Volvo',29000)")
+    cur.execute("INSERT INTO Book_classifier VALUES(5,'Bentley',350000)")
+    cur.execute("INSERT INTO Book_classifier VALUES(6,'Citroen',21000)")
+    cur.execute("INSERT INTO Book_classifier VALUES(7,'Hummer',41400)")
+    cur.execute("INSERT INTO Book_classifier VALUES(8,'Volkswagen',21600)")
+    cur.execute('SELECT * FROM Book_classifier')
+    allrows = cur.fetchall()
+    cur.execute("DROP TABLE Book_classifier")
+print(allrows)
+print (scifi)
